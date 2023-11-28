182ff0e09b292cccbfc3abb8e23fee8881f1cc54
DeviHafida <144782420+DeviHafida@users.noreply.github.com>
2023-11-28 22:08:19 +0800

    Add files via upload

---
+++ PRAK502-2310817220018-DeviHafidaAriyani.py
@@ -0,0 +1,9 @@
+def mutlak(angka):
+    return abs(angka)
+
+def hitung(nilai1, nilai2):
+    return abs(nilai1 - nilai2)
+
+a, c, b, d = map(int, input().split())
+hasil = hitung(a, b) + hitung(c, d)
+print(mutlak(hasil))
